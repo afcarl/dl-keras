42061f21c42f67d36603dd4af4088c7ae4772c0e
Rowel Atienza <rowel.atienza@gmail.com>
2017-10-09 13:19:59 +0800

    cifar 10

--- chapter3/cifar10_resnet.py
+++ chapter3/cifar10_resnet.py
@@ -1,20 +1,27 @@
-''' Trains a 6-layer ResNet on the MNIST dataset.
-    Gets to 99.4% test accuracy in 50 epochs
-    4sec per epoch on GTX 1080
+''' Trains a ResNet on the CIFAR10 dataset.
+    Greater than 87% test accuracy after 40 epochs
+    31per epoch on GTX 1080Ti
 '''
 
 from __future__ import print_function
 import keras
-from keras.layers import Dense, Conv2D, BatchNormalization, Activation, Dropout
-from keras.layers import MaxPooling2D
-from keras.optimizers import SGD, Adam
+from keras.layers import Dense, Conv2D, BatchNormalization, Activation
+from keras.layers import MaxPooling2D, AveragePooling2D, Input, Flatten
+from keras.optimizers import Adam
+from keras.callbacks import ModelCheckpoint, ReduceLROnPlateau
+from keras.preprocessing.image import ImageDataGenerator
+from keras.regularizers import l2
 from keras import backend as K
 from keras.models import Model
 from keras.datasets import cifar10
+import numpy as np
+import os
 
-batch_size = 128
+
+batch_size = 32
 num_classes = 10
-epochs = 200
+epochs = 100
+channel_axis = 3
 
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
 
@@ -25,6 +32,7 @@ if K.image_data_format() == 'channels_first':
     x_train = x_train.reshape(x_train.shape[0], channels, img_rows, img_cols)
     x_test = x_test.reshape(x_test.shape[0], channels, img_rows, img_cols)
     input_shape = (channels, img_rows, img_cols)
+    channel_axis = 1
 else:
     x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, channels)
     x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, channels)
@@ -41,38 +49,103 @@ print('y_train shape:', y_train.shape)
 y_train = keras.utils.to_categorical(y_train, num_classes)
 y_test = keras.utils.to_categorical(y_test, num_classes)
 
-xin = keras.layers.Input(shape=input_shape)
-x = xin
+xin = Input(shape=input_shape)
 
 filters = 64
-blocks = 8
-dropout = 0.3
-x = Conv2D(filters=filters, kernel_size=1, padding='same')(x)
-x = BatchNormalization()(x)
+blocks = 3
+sub_blocks = 3
+x = Conv2D(filters=filters, kernel_size=7, padding='same', strides=2,
+                    data_format=K.image_data_format(),
+                    kernel_initializer="he_normal", kernel_regularizer=l2(1e-4))(xin)
+x = BatchNormalization(axis=channel_axis)(x)
+x = Activation('relu')(x)        
+x = MaxPooling2D(pool_size=3, strides=2, padding="same")(x)
 for i in range(blocks):
-    # filters = 2*filters
-    y = Conv2D(filters=filters, kernel_size=3, padding='same')(x)
-    y = BatchNormalization()(y)
-    y = Activation('relu')(y)
-    y = Dropout(dropout)(y)
-    y = Conv2D(filters=filters, kernel_size=3, padding='same')(y)
-    y = BatchNormalization()(y)
-    # if i<blocks:
-    x = keras.layers.add([x, y])
-    x = Activation('relu')(x)
-    if i%2==0 and i>0:
-        x = MaxPooling2D(2)(x)
+    for j in range(sub_blocks):
+        strides = 1
+        is_first_layer_but_not_first_block = j==0 and i>0
+        if is_first_layer_but_not_first_block: 
+            strides = 2
+        y = Conv2D(filters=filters, kernel_size=3, padding='same', strides=strides,
+                    data_format=K.image_data_format(),
+                    kernel_initializer="he_normal", kernel_regularizer=l2(1e-4))(x)
+        y = BatchNormalization(axis=channel_axis)(y)
+        y = Activation('relu')(y)
+        y = Conv2D(filters=filters, kernel_size=3, padding='same',
+                    data_format=K.image_data_format(),
+                    kernel_initializer="he_normal", kernel_regularizer=l2(1e-4))(y)
+        y = BatchNormalization(axis=channel_axis)(y)
+        if is_first_layer_but_not_first_block: 
+            x = Conv2D(filters=filters, kernel_size=1, padding='same', strides=2,
+                    data_format=K.image_data_format(),
+                    kernel_initializer="he_normal", kernel_regularizer=l2(1e-4))(x)
+        x = keras.layers.add([x, y])
+        x = Activation('relu')(x)
 
-y = keras.layers.Flatten()(x)
-y = Dropout(dropout)(y)
-yout = Dense(num_classes, activation='softmax')(y)
+    filters = 2*filters
+    sub_blocks += 1
+
+x = AveragePooling2D(data_format=K.image_data_format())(x)
+y = Flatten()(x)
+yout = Dense(num_classes, activation='softmax', kernel_initializer="he_normal")(y)
 model = Model(inputs=[xin], outputs=[yout])
-model.compile(loss='categorical_crossentropy', optimizer=SGD(lr=1e-3),
+model.compile(loss='categorical_crossentropy', optimizer=Adam(),
               metrics=['accuracy'])
 model.summary()
 
-model.fit(x_train, y_train, epochs=epochs, batch_size=batch_size,
-          shuffle=True, validation_data=(x_test, y_test))
+save_dir = os.path.join(os.getcwd(), 'saved_models')
+model_name = "cifar10_resnet_model.hdf5" 
+# Save model and weights
+if not os.path.isdir(save_dir):
+    os.makedirs(save_dir)
+filepath = os.path.join(save_dir, model_name)
+checkpoint = ModelCheckpoint(filepath=filepath, verbose=1, save_best_only=True)
+lr_reducer = ReduceLROnPlateau(factor=np.sqrt(0.1), cooldown=0, patience=5, min_lr=0.5e-6)
+callbacks = [checkpoint, lr_reducer]
+
+data_augmentation = True
+
+if not data_augmentation:
+    print('Not using data augmentation.')
+    model.fit(x_train, y_train,
+              batch_size=batch_size,
+              epochs=epochs,
+              validation_data=(x_test, y_test),
+              shuffle=True,
+              callbacks=callbacks)
+else:
+    print('Using real-time data augmentation.')
+    # This will do preprocessing and realtime data augmentation:
+    datagen = ImageDataGenerator(
+        featurewise_center=False,  # set input mean to 0 over the dataset
+        samplewise_center=False,  # set each sample mean to 0
+        featurewise_std_normalization=False,  # divide inputs by std of the dataset
+        samplewise_std_normalization=False,  # divide each input by its std
+        zca_whitening=False,  # apply ZCA whitening
+        rotation_range=0,  # randomly rotate images in the range (degrees, 0 to 180)
+        width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
+        height_shift_range=0.1,  # randomly shift images vertically (fraction of total height)
+        horizontal_flip=True,  # randomly flip images
+        vertical_flip=False)  # randomly flip images
+
+    # Compute quantities required for featurewise normalization
+    # (std, mean, and principal components if ZCA whitening is applied).
+    datagen.fit(x_train)
+
+    # Fit the model on the batches generated by datagen.flow().
+    model.fit_generator(datagen.flow(x_train, y_train, batch_size=batch_size),
+                        steps_per_epoch=x_train.shape[0] // batch_size,
+                        validation_data=(x_test, y_test),
+                        epochs=epochs, verbose=1, workers=4,
+                        callbacks=callbacks)
+
+
 score = model.evaluate(x_test, y_test, verbose=0)
+# Evaluate model with test data set and share sample prediction results
+score = model.evaluate_generator(datagen.flow(x_test, y_test,
+                                      batch_size=batch_size,
+                                      shuffle=False),
+                                      steps=x_test.shape[0] // batch_size,
+                                      workers=4)
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
