b616b539b3e6e47486e86dea4c1f5271cb1a85f9
Rowel Atienza <rowel.atienza@gmail.com>
2018-01-05 16:04:23 +0800

    autoencoder fixes

--- chapter4-autoencoders/autoencoder-mnist-4.2.1.py
+++ chapter4-autoencoders/autoencoder-mnist-4.2.1.py
@@ -1,18 +1,28 @@
-'''
+'''Example of autoencoder model on MNIST dataset
+
+This autoencoder has modular design. The encoder, decoder and autoencoder
+are 3 models that share weights. For example, after training the
+autoencoder, the encoder can be used to  generate latent vectors
+of input data for low-dim visualization like PCA or TSNE.
+
 Author: Rowel Atienza
 Project: https://github.com/roatienza/dl-keras
 Dependencies: keras with tensorflow backend
 Usage: python3 <this file>
 '''
 
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
 import numpy as np
 import keras
-from keras.layers import Activation, Dense, Dropout, Input, BatchNormalization
+from keras.layers import Activation, Dense, Input, BatchNormalization
 from keras.layers import Conv2D, MaxPooling2D, Flatten
 from keras.layers import Reshape, Conv2DTranspose, UpSampling2D
 from keras.models import Model
 from keras.datasets import mnist
 from keras.utils import plot_model
+from keras import backend as K
 import matplotlib.pyplot as plt
 
 # MNIST dataset
@@ -28,78 +38,81 @@ x_test = x_test.astype('float32') / 255
 input_shape = (image_size, image_size, 1)
 batch_size = 128
 kernel_size = 3
-pool_size = 2
-dropout = 0.4
 filters = 16
 latent_dim = 16
 
-# Build the Autoencoder model
-# First build the Encoder model
+# Build the Autoencoder Model
+# First build the Encoder Model
 inputs = Input(shape=input_shape, name='encoder_input')
 x = inputs
-# Stack of BN-ReLU-Conv2D-MaxPooling blocks
+# Stack of BN-ReLU-Conv2D-MaxPooling2D blocks
 for i in range(2):
     x = BatchNormalization()(x)
     x = Activation('relu')(x)
-    filters = filters * 2
-    x = Conv2D(filters=filters, kernel_size=kernel_size,
+    filters *= 2
+    x = Conv2D(filters=filters,
+               kernel_size=kernel_size,
                padding='same')(x)
     x = MaxPooling2D()(x)
 
-# Shape info needed to build decoder model
-shape = x.shape.as_list()
+# Shape info needed to build Decoder Model
+shape = K.int_shape(x)
 
 # Generate a 16-dim latent vector
 x = Flatten()(x)
 latent = Dense(latent_dim, name='latent_vector')(x)
 
-# Instantiate Encoder model
+# Instantiate Encoder Model
 encoder = Model(inputs, latent, name='encoder')
 encoder.summary()
 plot_model(encoder, to_file='encoder.png', show_shapes=True)
 
-# Build the Decoder model
+# Build the Decoder Model
 latent_inputs = Input(shape=(latent_dim,), name='decoder_input')
 x = Dense(shape[1]*shape[2]*shape[3])(latent_inputs)
 x = Reshape((shape[1], shape[2], shape[3]))(x)
 
-# Stack of BN-ReLU-Transposed Conv2D-UpSampling blocks
+# Stack of BN-ReLU-Transposed Conv2D-UpSampling2D blocks
 for i in range(2):
     x = BatchNormalization()(x)
     x = Activation('relu')(x)
-    x = Conv2DTranspose(filters=filters, kernel_size=kernel_size,
+    x = Conv2DTranspose(filters=filters,
+                        kernel_size=kernel_size,
                         padding='same')(x)
     x = UpSampling2D()(x)
-    filters = int(filters / 2)
+    filters //= 2
 
-x = Conv2DTranspose(filters=1, kernel_size=kernel_size,
+x = Conv2DTranspose(filters=1,
+                    kernel_size=kernel_size,
                     padding='same')(x)
 
 outputs = Activation('sigmoid', name='decoder_output')(x)
 
-# Instantiate Decoder model
+# Instantiate Decoder Model
 decoder = Model(latent_inputs, outputs, name='decoder')
 decoder.summary()
 plot_model(decoder, to_file='decoder.png', show_shapes=True)
 
 # Autoencoder = Encoder + Decoder
-# Instantiate Autoencoder model
-autoencoder = Model(inputs, decoder(encoder(inputs)), name='autodecoder')
+# Instantiate Autoencoder Model
+autoencoder = Model(inputs, decoder(encoder(inputs)), name='autoencoder')
 autoencoder.summary()
 plot_model(autoencoder, to_file='autoencoder.png', show_shapes=True)
 
 # Mean Square Error (MSE) loss function, Adam optimizer
 autoencoder.compile(loss='mse', optimizer='adam')
 
-# Train the autoencoder for 1 epoch
-autoencoder.fit(x_train, x_train,
+# Train the autoencoder
+autoencoder.fit(x_train,
+                x_train,
                 validation_data=(x_test, x_test),
-                epochs=1, batch_size=batch_size)
+                epochs=1,
+                batch_size=batch_size)
 
 # Predict the Autoencoder output from test data
 x_decoded = autoencoder.predict(x_test)
 
-# Display the 1st 8 input and decoded images
+# Display the 1st 8 test input and decoded images
 imgs = np.concatenate([x_test[:8], x_decoded[:8]])
 imgs = imgs.reshape((4, 4, image_size, image_size))
 imgs = np.vstack([np.hstack(i) for i in imgs])
